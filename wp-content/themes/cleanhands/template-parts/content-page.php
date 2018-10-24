8f42bdc8a81bec9f04650764a81a8c6d64758ff3
lexinamer <lexinamer@gmail.com>
2018-10-24 14:04:39 -0400

    Adding template pages and parts

--- wp-content/themes/cleanhands/template-parts/content-page.php
+++ wp-content/themes/cleanhands/template-parts/content-page.php
@@ -12,6 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_subtitle('<h3>', '</h3>'); ?>
 	</header><!-- .entry-header -->
 
 	<?php cleanhands_post_thumbnail(); ?>
